625af1c7d7c8241f50a4f3171be299741c02d954
shuhelahm <shuhel037@gmail.com>
2022-03-16 00:23:48 +0600

    cloud facilities section added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './Home.css';
 import polygon2 from '../../img/Polygon 2.png';
 import  '../../img/Rectangle 5.png';
-import calendar from  '../../img/calender.png';
+import google from  '../../img/google-2015 1.png';
+import facebook from  '../../img/facebook.png';
+import airbnb from  '../../img/airbnb 1.png';
+import netflix from  '../../img/netflix.png';
+import amazon from  '../../img/amazon.png';
+import grab from  '../../img/grab-logo.png';
+import bill from '../../img/Group 79.png'
+import schedule from '../../img/Group 80.png'
+import tracking from '../../img/Group 81.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Home = () => {
     return (
@@ -19,7 +28,7 @@ const Home = () => {
         <div className='banner-right d-grid'>
             <div>
                 <div>
-                    <img src={calendar} alt="" />
+                <FontAwesomeIcon icon="fa-solid fa-calendar" />
                 </div>
                 <div>
 
@@ -31,6 +40,45 @@ const Home = () => {
         </div>     
         </div>
         </section> 
+
+        {/* worldwide trusted companies */}
+
+        <section>
+            <h6 className='trusted-companies-heading'>Trusted by 5,000 Companies Worldwide</h6>
+            <div className='trusted-companies-section d-grid'>
+                <div><img src={google} alt="" /></div>
+                <div><img src={netflix} alt="" /></div>
+                <div><img src={airbnb} alt="" /></div>
+                <div><img src={amazon} alt="" /></div>
+                <div><img src={facebook} alt="" /></div>
+                <div><img src={grab} alt="" /></div> 
+            </div>
+        </section>
+
+        {/* all-in-one cloud software section  */}
+        <section>
+            <div className='text-center cloud-software-top'>
+                <h4 className='blue-all-in-one'>All-In-One <span className='orange-cloud-software'>Cloud Software</span></h4>
+                <p>Skilline is one powerful online software suite that combines all the tools needed to run a successful school or office</p>
+            </div>
+            <div className='d-grid cloud-software-facilities'>
+                <div className='cloud-software-facility-card'>
+                    <img src={bill} alt="" />
+                    <h5>Online Billing, Invoicing, &#38; Contracts</h5> 
+                    <p>Simple and secure control of your organization's financial and legal transactions. Send customized invoices and contracts</p>
+                </div>
+                <div className='cloud-software-facility-card'>
+                    <img src={schedule} alt="" />
+                    <h5>Easy Scheduling and Attendane Tracking</h5> 
+                    <p>Schedule and reserve at one campus or multiple campuses. keep detailed records of student attendane.</p>
+                </div>
+                <div className='cloud-software-facility-card'>
+                    <img src={tracking} alt="" />
+                    <h5>Customer Tracking</h5> 
+                    <p>Automate and track emails to individuals or groups. Skilline's built-in-system helps organize your organization</p>
+                </div>
+            </div>
+        </section>
         </>
     );
 };
